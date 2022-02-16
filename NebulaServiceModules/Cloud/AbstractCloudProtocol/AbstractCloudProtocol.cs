a7d2945826073c380c461cac9cbd1afe8d95b404
viniciof <vinicio.flores@intel.com>
2022-02-16 15:54:08 -0600

    Basic building blocks for solution's SOA structure

---
+++ NebulaServiceModules/Cloud/AbstractCloudProtocol/AbstractCloudProtocol.cs
@@ -0,0 +1,13 @@
+﻿using NebulaBase.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NebulaServiceModules
+{
+    public abstract class AbstractCloudProtocol : ICloudProtocol
+    {
+    }
+}
---
+++ NebulaServices/CloudProtocols/CloudService.cs
@@ -0,0 +1,19 @@
+﻿using NebulaBase.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NebulaServices
+{
+    /// <summary>
+    /// Implements protocol to provide a single view of the Service Oriented Architecture 
+    /// layer while keeping the abstraction and virtualisation of the project overall. 
+    /// Initial Behaviour that distributed protocols, such as a Gossip one, will inherit from here.
+    /// </summary>
+    public class CloudService : AbstractNebulaService
+    {
+
+    }
+}
